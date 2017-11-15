1a5dafd4f501a1020a8addc6cbc7f824b504831a
Mawutor Ama Abalo <amiabalo@Mawutors-MacBook-Pro.local>
2017-11-16 02:04:45 +0400

    Renaming to SignUpView

--- Hungry-NYUAD/SignUpView.swift
+++ Hungry-NYUAD/SignUpView.swift
@@ -7,20 +7,35 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuthUI
+import FirebaseGoogleAuthUI
+import FirebaseFacebookAuthUI
+import FirebaseTwitterAuthUI
+import FirebasePhoneAuthUI
+import GoogleSignIn
 
-class SignUpView: UIViewController {
-    
+
+class SignUpView: UIViewController, GIDSignInUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        
+        // TODO(developer) Configure the sign-in button look/feel
+        // ...
+        
+        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
 }
 
 
