87b86627d321dffa3d631f04801e1a0059b2790b
ANDREW <aac511@nyu.edu>
2017-11-16 15:01:16 +0400

    Started code to deal with the management of the orders.

---
+++ Hungry-NYUAD/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Hungry-NYUAD
+//
+//  Created by Andrew Callender on 11/16/17.
+//  Copyright © 2017 Software Engineering Group. All rights reserved.
+//
+
+import Foundation
---
+++ Hungry-NYUAD/GroupDetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OrderManViewController.swift
+//  Hungry-NYUAD
+//
+//  Created by Andrew Callender on 11/16/17.
+//  Copyright © 2017 Software Engineering Group. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class GroupDetailsViewController : UIViewController {
+    
+    //MARK: Properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+        
+}
