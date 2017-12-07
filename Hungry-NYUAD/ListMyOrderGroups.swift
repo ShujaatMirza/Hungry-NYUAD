2fceb1a5185f1cedf449915159fd6acfd3d8d793
Shujaat Mirza <Shujaat@Muhammad-MBP.local>
2017-12-07 14:44:44 +0400

    Orders Created by a user working. Fixes #18

--- Hungry-NYUAD/ListMyOrderGroups.swift
+++ Hungry-NYUAD/ListMyOrderGroups.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import Firebase
 
 class ListMyOrderGroups: UITableViewController {
-
+    
+    var orderGroupObjectToSend : OrderGroup? = nil
+    var orderGroups = [OrderGroup]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.rowHeight = UITableViewAutomaticDimension;
+        self.tableView.estimatedRowHeight = 44.0;
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let databaseRef = Database.database().reference().child("order_group")
+        databaseRef.observe(DataEventType.value, with: { (snapshot) in
+            
+            //if the reference have some values
+            if snapshot.childrenCount > 0 {
+                
+                //clearing the list
+                self.orderGroups.removeAll()
+                
+                //iterating through all the values
+                for ordergroups in snapshot.children.allObjects as! [DataSnapshot] {
+                    //getting values
+                    let ordergroupObject = ordergroups.value as? [String: AnyObject]
+                    //Condition to check if the Order Group belongs to this owner
+                    if ((ordergroupObject?["ownerId"] as! String) == Auth.auth().currentUser?.uid){
+                        
+                        let ordergroupName  = ordergroupObject?["name"]
+                        let ordergroupId  = ordergroupObject?["id"]
+                        let ordergroupRestaurant = ordergroupObject?["restaurant"]
+                        let ordergroupOwnerId = ordergroupObject?["ownerId"]
+                        let ordergroupIsPlaced = ordergroupObject?["IsPlaced"]
+                        let ordergroupIsDelivered = ordergroupObject?["IsDelivered"]
+                        let ordergroupIsCompleted = ordergroupObject?["IsCompleted"]
+                        let ordergrouphasReachedCapacity = ordergroupObject?["hasReachedCapacity"]
+                        let ordergroupnumMembers = ordergroupObject?["numMembers"]
+                        
+                        let orderGroup = OrderGroup(id: (ordergroupId as! String?)!, name: (ordergroupName as! String?)!, restaurant: (ordergroupRestaurant as! String?)!, ownerId: (ordergroupOwnerId as! String?)!, IsPlaced: (ordergroupIsPlaced as! Bool?)!, IsDelivered: (ordergroupIsDelivered as! Bool?)!, IsCompleted: (ordergroupIsCompleted as! Bool?)!, hasReachedCapacity: (ordergrouphasReachedCapacity as! Bool?)!, numMembers: (ordergroupnumMembers as! Int?)!
+                        )
+                        //self.orderGroupObjectToSend = orderGroup
+                        //print("The object capacity is as follows " + (self.orderGroupObjectToSend?.hasReachedCapacity.description)!)
+                        //appending it to list
+                        self.orderGroups.append(orderGroup!)
+                    }
+                    
+                }
+                
+                //reloading the tableview
+                self.tableView.reloadData()
+            }
+            print(self.orderGroups.count)
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,24 +71,51 @@ class ListMyOrderGroups: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return orderGroups.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let Cell = "ListMyOrderGroupsTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? ListMyOrderGroupsTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of ListMyOrderGroupsTableViewCell.")
+        }
+        // Fetches the appropriate meal for the data source layout.
+        let orderGroup = orderGroups[indexPath.row]
+        
+        cell.idLabel.text = orderGroup.id
+        cell.restaurantLabel.text = orderGroup.restaurant
+        cell.OrderNameLabel.text = orderGroup.name
+        
+        
+        //setting the data to pass to the next view-controller to the restaurant name
+        
         return cell
     }
-    */
-
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showGroupData" {
+            
+            if let ExchangeViewData = segue.destination as? GroupDetailsViewController{
+                //ExchangeViewData.orderGroupObject = self.orderGroupObjectToSend!
+                let index = tableView.indexPathForSelectedRow?.row
+                ExchangeViewData.orderGroupObject = orderGroups[index!]
+                print(ExchangeViewData.orderGroupObject.id)
+                print(ExchangeViewData.orderGroupObject.restaurant)
+                print(ExchangeViewData.orderGroupObject.hasReachedCapacity.description)
+            }
+        }
+        
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -93,3 +162,4 @@ class ListMyOrderGroups: UITableViewController {
     */
 
 }
+
