29dff42b639654c9dba2a8801d66f0ea44a174ee
jma587 <jma587@nyu.edu>
2017-12-10 16:57:59 +0400

    Allowing user to review order. Fixes #21

---
+++ Hungry-NYUAD/ReviewOrderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewOrderTableViewCell.swift
+//  Hungry-NYUAD
+//
+//  Created by Mawutor Ama Abalo on 12/10/17.
+//  Copyright © 2017 Software Engineering Group. All rights reserved.
+//
+
+import UIKit
+
+class ReviewOrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
