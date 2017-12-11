95626020ef94b6bec3b13e3b04ba4d29ec646a46
ANDREW <aac511@nyu.edu>
2017-12-11 23:54:53 +0400

    Minor changes

--- Hungry-NYUAD/ReviewOrderViewController.swift
+++ Hungry-NYUAD/ReviewOrderViewController.swift
@@ -105,16 +105,4 @@ class ReviewOrderViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return selectedItems?.count ?? 0
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
